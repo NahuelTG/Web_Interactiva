517b9090db1e007b58d1f719eb9202cb5951d6d4
NahuelTG <nahueltorrezg@gmail.com>
2025-04-01 17:28:14 -0400

    add: svg Recorridos

---
+++ src/assets/svg/RecorridoVirtual.jsx
@@ -0,0 +1,14 @@
+export const RecorridoVirtual = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      version="1.0"
+      width="346.000000pt"
+      height="225.000000pt"
+      viewBox="0 0 346.000000 225.000000"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <g transform="translate(0.000000,225.000000) scale(0.100000,-0.100000)" fill="#fffff" stroke="none"></g>
+    </svg>
+  )
+}
