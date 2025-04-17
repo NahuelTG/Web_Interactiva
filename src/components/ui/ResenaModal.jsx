e02e4a02fe388dd66372181c6cd9055174871421
NahuelTG <nahueltorrezg@gmail.com>
2025-04-17 11:47:22 -0400

    update}: information modal

--- src/components/ui/ResenaModal.jsx
+++ src/components/ui/ResenaModal.jsx
@@ -3,6 +3,7 @@
 import PropTypes from 'prop-types'
 import Modal from './Modal'
 import resenaHTML_1 from '../../assets/data/resena-summergo.html1'
+import resenaHTML_2 from '../../assets/data/resena-summergo.html2'
 import resenaTable from '../../assets/data/resena-summergo.table.html'
 import './Modal.css'
 
@@ -13,6 +14,7 @@ const ResenaModal = ({ isOpen, onClose }) => {
         <div dangerouslySetInnerHTML={{ __html: resenaHTML_1 }} />
         <h3 style={{ color: '#00d4ff', marginTop: '2rem' }}>Proyectos Institucionales y Microdocumentales</h3>
         <div className="responsive-table" dangerouslySetInnerHTML={{ __html: resenaTable }} />
+        <div dangerouslySetInnerHTML={{ __html: resenaHTML_2 }} />
       </div>
     </Modal>
   )
