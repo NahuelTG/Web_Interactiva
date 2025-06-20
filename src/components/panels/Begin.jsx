1a24f0a89093625561349aa076cdb263a870005c
NahuelTG <nahueltorrezg@gmail.com>
2025-06-19 21:50:23 -0400

    style: button sound

--- src/components/panels/Begin.jsx
+++ src/components/panels/Begin.jsx
@@ -1,10 +1,12 @@
 import Modal from '../ui/Modal'
 import ScrollHint from '../ui/ScrollHint'
+import { useWindowSize } from '../../hooks/useWindowSize'
 import { useState } from 'react'
 import './Begin.css'
 
 export const Begin = () => {
   const [isModalOpen, setModalOpen] = useState(true)
+  const { width } = useWindowSize()
 
   const handleContinuar = () => {
     setModalOpen(false)
@@ -15,10 +17,18 @@ export const Begin = () => {
       <Modal isOpen={isModalOpen} onClose={handleContinuar} begin={true}>
         <div className="begin-modal-content">
           <h1 className="welcome-title">SUMÉRGETE</h1>
-          <p className="welcome-text">
-            Te damos la bienvenida a <strong>Summergo</strong>. Estás a punto de entrar a un mundo interactivo en 3D. Para comenzar tu
-            viaje, desliza hacia arriba.
-          </p>
+          {width < 768 ? (
+            <p className="welcome-text">
+              Te damos la bienvenida a <strong>Summergo</strong>. Estás a punto de entrar a un mundo interactivo en 3D. Para comenzar tu
+              viaje, haz clic en <strong>Empezar</strong> y <strong>desliza hacia abajo.</strong>.
+            </p>
+          ) : (
+            <p className="welcome-text">
+              Te damos la bienvenida a <strong>Summergo</strong>. Estás a punto de entrar a un mundo interactivo en 3D. Para comenzar tu
+              viaje, haz clic en <strong>Empezar</strong> y <strong>desplázate hacia arriba</strong>
+            </p>
+          )}
+
           <ScrollHint />
         </div>
 
