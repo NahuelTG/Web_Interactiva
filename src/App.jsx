cd5e2537f5e17b392c55dae7a51d644456c412bf
NahuelTG <nahueltorrezg@gmail.com>
2025-06-24 15:54:08 -0400

    revert: scroll hint in screen

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import './App.css'
 import Scene from './components/Scene'
 import { Footer } from './components/ui/Footer'
 import { Navbar } from './components/ui/Navbar'
-import { Begin } from './components/panels/Begin'
 import SoundButton from './components/buttons/SoundButton'
+import ScrollHint from './components/ui/ScrollHint'
 
 function App() {
   const [scrollOffset, setScrollOffset] = useState(0)
@@ -15,8 +15,8 @@ function App() {
       <Scene onScrollUpdate={setScrollOffset} />
       <Navbar />
       <Footer scrollOffset={scrollOffset} />
-      <Begin />
       <SoundButton />
+      <ScrollHint />
     </div>
   )
 }
--- src/components/ui/ScrollHint.jsx
+++ src/components/ui/ScrollHint.jsx
@@ -1,14 +1,25 @@
 // src/components/ui/ScrollHint.jsx
+import { useState, useEffect } from 'react'
 import useWindowWidth from '../../hooks/useWindowWidth'
 import { Mouse } from '../../assets/svg/Mouse'
 import { IndexFinger } from '../../assets/svg/IndexFinger'
 import './ScrollHint.css'
+
 const ScrollHint = () => {
+  const [isVisible, setIsVisible] = useState(true)
   const width = useWindowWidth()
   const isMobile = width <= 768
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(false)
+    }, 5000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
   return (
-    <div className={`scroll-hint `}>
+    <div className={`scroll-hint ${!isVisible ? 'scroll-hint--hidden' : ''}`}>
       <div className="scroll-hint__icon-container">{isMobile ? <IndexFinger /> : <Mouse />}</div>
     </div>
   )
